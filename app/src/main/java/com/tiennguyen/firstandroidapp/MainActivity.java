959a22787ea4fd9697a7c58a772c1553728aa2c6
Tien Nguyen <tiennguyen5118@gmail.com>
2018-08-14 13:42:40 +0700

    First project

---
+++ app/src/main/java/com/tiennguyen/firstandroidapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tiennguyen.firstandroidapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
